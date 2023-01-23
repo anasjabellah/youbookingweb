5073538ebd445430b8831fafed49f2fee0b71ef3
anas jabellah <jablcodiing@gmail.com>
2023-01-23 11:34:30 +0100

    Update

--- YouBookingWebApplication.java
+++ YouBookingWebApplication.java
@@ -2,6 +2,7 @@ package com.example.youbookingweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class YouBookingWebApplication {
